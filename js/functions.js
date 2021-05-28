def3577340c911f0188d6c629461fbb34c4103f1
michal sladewski <michal.sladewski@gmail.com>
2021-05-28 18:01:38 +0200

    Add JS variables

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to nożyce, to Wygrywasz!');
