71118b26c28a092f86561b648949524c6071555d
michal sladewski <michal.sladewski@gmail.com>
2021-05-29 09:11:46 +0200

    Update JS - playerMove

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,16 @@ printMessage('Mój ruch: ' + computerMove);
 
 // Zapisywanie odpowiedzi gracza 
 
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
